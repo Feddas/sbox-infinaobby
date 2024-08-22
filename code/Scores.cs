cc948914f9d1d5189c934a277f8fb6ce80322d63
Feddas <feddas00@gmail.com>
2024-08-22 12:58:08 -0700

    Leaderboard works now. write up on https://sbox.game/feddas/infinaobby/news/alpha-released-548c7f10/leaderboard

--- code/Scores.cs
+++ code/Scores.cs
@@ -3,17 +3,21 @@ using System.Threading.Tasks;
 
 public sealed class Scores : Component
 {
+	private readonly string keyScore = "score";// Don't use "score_num" + DaysSince.ReleaseDate;, instead (eventually when it works) use https://sbox.game/feddas/infinaobby/stats/leaderboards/help/api
+
 	/// <summary> The maximum z value the player has obtained. </summary>
 	[ReadOnly, Property] public float MaxHeight { get; private set; } = float.MinValue;
 	[ReadOnly, Property] public float MaxScore { get; private set; } = float.MinValue;
 
+	// sbox require component: https://github.com/Facepunch/sbox-issues/issues/4659
 	[RequireComponent] private Health Health { get; set; }
 
 	public string Instructions { get; private set; } = "Use cubes to jump higher than anyone else on the leaderboards.";
 
 	protected override void OnStart()
 	{
-		MaxScore = (float)Sandbox.Services.Stats.GetLocalPlayerStats( "infinaobby" ).Get( "score" ).Value;
+		MaxScore = (float)Sandbox.Services.Stats.GetLocalPlayerStats( "infinaobby" ).Get( keyScore ).Value;
+		Log.Info( "stats has MaxScore of " + MaxScore );
 		_ = RemoveInstructions( 1600 );
 	}
 
@@ -43,7 +47,8 @@ public sealed class Scores : Component
 				MaxScore = MaxHeight;
 
 				// update leaderboard
-				Sandbox.Services.Stats.SetValue( "score_num" + DaysSince.ReleaseDate, MaxScore );
+				Sandbox.Services.Stats.SetValue( keyScore, MaxScore );
+				Log.Info( "set stats MaxScore to " + MaxScore );
 			}
 
 		}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# goal
+
+Infinite obstacle course. Use Unity3d competitor called s&box.
+
+# details
+
+Full game details at https://sbox.game/feddas/infinaobby.
+This game was an entry into [Game Jam 2](https://sbox.game/c/gamejam2).
+
+# s&box
+
+This was my first project using s&box. s&box is a Unity3d competitor that:
+1. Relies on scripting with C#.
+2. When not exported, hosts multiplayer servers for free, Roblox style.
+3. Allows export, exporting this project was over 2GB.
+
+# Dev TODO
+
+1. Change procedural level generation to be based on how many seconds a player jump lasts for, from leaving ground to touching ground again. Use that seconds value to change the length of the platform cubes and their interval. Currently all platform cubes are the same scale, spawned at an increasing interval. The interval was guesstimated from trial and error.
+
+# repos
+
+https://github.com/feddas/sbox-infinaobby
